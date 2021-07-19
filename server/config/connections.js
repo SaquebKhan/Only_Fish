61706cd9b0f0289b7f27576fb7baa695672706bc
Jamie Kaczor <dignanjk@aol.com>
2021-07-19 09:31:09 -0400

    pathing

--- server/config/connections.js
+++ server/config/connections.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/onlyFish', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/Only_Fish', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
