323b9573657a311bda9e1e989ae3f8e0befc8bef
SaquebKhan <82435053+SaquebKhan@users.noreply.github.com>
2021-07-19 17:26:06 -0400

    Update

--- client/src/pages/Contact/Contact.js
+++ client/src/pages/Contact/Contact.js
@@ -1,12 +1,14 @@
 import React from 'react';
-
+import {  Container } from "react-bootstrap"
 export default function Contact() {
   return (
     <div>
+      <Container>
       <h1>Contact Page</h1>
       <p>
-       
+       If you have any questions please email khansaqueb@gmail.com
       </p>
+    </Container>
     </div>
   );
 }
